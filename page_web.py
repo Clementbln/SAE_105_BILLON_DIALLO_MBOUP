5c390538dbd15283f29c18f875c637ea9a2af6b7
clement <clementbillon936@gmail.com>
2024-11-26 11:47:05 +0100

    ajout code pour generer la page web

---
+++ page_web.py
@@ -0,0 +1,9 @@
+graphe= "/home/etudiant/SAE_105_BILLON_DIALLO_MBOUP/logarithmic_spiral.png"
+
+html = f"""<h1>Graphe debit riviere</h1>
+        <p><img src="{graphe}" alt="" /></p>"""
+        
+fichier = open("html/index.html", mode ='w', encoding="utf-8")
+fichier.write(html)
+fichier.close()
+    
